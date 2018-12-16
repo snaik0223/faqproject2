a05bea6496edc3527a60884501df29366f8ee5ba
Shamix <nsammi11@gmail.com>
2018-12-15 19:06:10 -0500

    Task: Added Answer migrate, model and made answer seeder and added answer test

--- app/Question.php
+++ app/Question.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     public function user()
-{
+    {
     return $this->belongsTo('App\User');
-}
+    }
+    public function answers()
+    {
+        return $this->hasMany('App\Answer');
+    }
 }
