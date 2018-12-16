83e390605186d1a9cfc798a8ab0c5f9d0e90ee18
Shamix <nsammi11@gmail.com>
2018-12-16 13:03:58 -0500

    Task: Added answer seeding loop

--- database/seeds/AnswersTableSeeder.php
+++ database/seeds/AnswersTableSeeder.php
@@ -11,14 +11,15 @@ class AnswersTableSeeder extends Seeder
      */
     public function run()
     {
-            $users = App\User::inRandomOrder();
+        $users = App\User::inRandomOrder();
         for ($i = 1; $i <= 16; $i++) {
-            $users->each(function ($user){
+            $users->each(function ($user) {
                 $question = App\Question::inRandomOrder()->first();
                 $answer = factory(\App\Answer::class)->make();
                 $answer->user()->associate($user);
                 $answer->question()->associate($question);
                 $answer->save();
             });
+        }
     }
 }
